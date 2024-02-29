7a5a2be851b2ebf7305dfce51c786a9b85157b3d
KotenkoNata <kotenkonata8@gmail.com>
2024-02-29 15:03:48 +0100

    feat: Using Express Router

--- app.js
+++ app.js
@@ -2,26 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/',(req,res, next)=>{
-    console.log("always");
-    next()
-});
-app.use('/add-product',(req,res, next)=>{
-    console.log("another middleware!");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/');
-})
-app.use('/',(req,res, next)=>{
-    console.log("another middleware2");
-    res.send('<h1>Hello from Express</h1>');
-});
 
 app.listen(3000);
 

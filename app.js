6903d2573fc42a9668fb2a8b04819fbc2615144d
KotenkoNata <kotenkonata8@gmail.com>
2024-02-29 14:52:37 +0100

    feat: Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded());
 
 app.use('/',(req,res, next)=>{
     console.log("always");
@@ -8,8 +11,13 @@ app.use('/',(req,res, next)=>{
 });
 app.use('/add-product',(req,res, next)=>{
     console.log("another middleware!");
-    res.send('<h1>Add product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
+})
 app.use('/',(req,res, next)=>{
     console.log("another middleware2");
     res.send('<h1>Hello from Express</h1>');

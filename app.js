8a838f729abd97e212cc29fe101073e63f43e956
KotenkoNata <kotenkonata8@gmail.com>
2024-02-29 14:38:00 +0100

    feat: Handling Different Routes

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res, next)=>{
-    console.log("middleware");
-    next();
+app.use('/',(req,res, next)=>{
+    console.log("always");
+    next()
 });
-
-app.use((req,res, next)=>{
-    console.log("another middleware");
+app.use('/add-product',(req,res, next)=>{
+    console.log("another middleware!");
+    res.send('<h1>Add product page</h1>');
+});
+app.use('/',(req,res, next)=>{
+    console.log("another middleware2");
     res.send('<h1>Hello from Express</h1>');
 });
 

0bf5bb930f77f5b1494242a8dc265656cd028e5c
KotenkoNata <kotenkonata8@gmail.com>
2024-03-01 13:21:20 +0100

    feat: Serving HTML Pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 // admin/add-product=>GET
 router.get('/add-product',(req,res, next)=>{
-    console.log("another middleware!");
-    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
 });
 // admin/add-product=>POST
 router.post('/add-product',(req,res,next)=>{

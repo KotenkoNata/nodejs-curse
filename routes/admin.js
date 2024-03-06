bb931cd9abece5a0085d8eab0d1416aeda03bf6e
KotenkoNata <kotenkonata8@gmail.com>
2024-03-06 15:55:39 +0100

    feat: Finishing the Pug Template

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const products = [];
 // admin/add-product=>GET
 router.get('/add-product',(req,res, next)=>{
-    res.render('add-product', {pageTitle: "Add Product"})
+    res.render('add-product', {pageTitle: "Add Product", path: '/admin/add-product'})
 });
 // admin/add-product=>POST
 router.post('/add-product',(req,res,next)=>{

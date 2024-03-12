f310c690bfa00335004886f562f3ae5ce05f047c
KotenkoNata <kotenkonata8@gmail.com>
2024-03-12 15:09:20 +0100

    feat: Adding Controllers

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next)=>{
@@ -21,5 +21,5 @@ app.use((req, res, next)=>{
 })
 
 
-app.listen(3000);
+app.listen(3200);
 

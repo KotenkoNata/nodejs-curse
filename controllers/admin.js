d4c8a1fba63c933b6973d6b798e5bf51d20c417d
KotenkoNata <kotenkonata8@gmail.com>
2024-03-14 14:59:21 +0100

    feat: Registering the Routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req,res, next)=> {
     res.render('admin/add-product', {
@@ -16,16 +16,13 @@ exports.postAddProduct = (req,res,next)=>{
     res.redirect('/');
 };
 
-exports.getProducts = (req,res, next)=>{
+exports.getProducts = (req,res,next)=>{
     Product.fetchAll((products)=>{
-        res.render('shop/product-list',
+        res.render('admin/products',
             {
                 prods: products,
-                pageTitle: 'Shop',
-                path:'/',
-                hasProducts: products.length > 0,
-                activeShop: true,
-                productCSS: true,
+                pageTitle: 'Admin Products',
+                path:'/admin/products'
             });
     });
 };

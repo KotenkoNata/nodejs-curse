fa301d890ca85243333dfee324c6b3cbd370e998
KotenkoNata <kotenkonata8@gmail.com>
2024-03-19 14:32:29 +0100

    feat: Linking to the Edit Page

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,49 +1,52 @@
-const Product = require("../models/product");
+const Product = require('../models/product');
 
-exports.getAddProduct = (req,res, next)=> {
+exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
-        pageTitle: "Add Product",
+        pageTitle: 'Add Product',
         path: '/admin/add-product',
-        editing: false,
+        editing: false
     });
 };
 
-exports.postAddProduct = (req,res,next)=>{
+exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
-    const description = req.body.description;
     const price = req.body.price;
+    const description = req.body.description;
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
 
-exports.getEditProduct = (req,res, next)=> {
+exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit;
-    if(!editMode){
-        return res.redirect('/')
+    if (!editMode) {
+        return res.redirect('/');
     }
     const prodId = req.params.productId;
     Product.findById(prodId, product => {
-        if(!product){
+        if (!product) {
             return res.redirect('/');
         }
         res.render('admin/edit-product', {
-            pageTitle: "Edit Product",
+            pageTitle: 'Edit Product',
             path: '/admin/edit-product',
             editing: editMode,
-            product: product,
+            product: product
         });
-    })
+    });
 };
 
-exports.getProducts = (req,res,next)=>{
-    Product.fetchAll((products)=>{
-        res.render('admin/products',
-            {
-                prods: products,
-                pageTitle: 'Admin Products',
-                path:'/admin/products'
-            });
+exports.postEditProduct = (req, res, next)=>{
+
+}
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
+        });
     });
 };

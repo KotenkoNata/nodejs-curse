eba1394468d5415d6fdba7f3e3cb5e16cbcd3e6b
KotenkoNata <kotenkonata8@gmail.com>
2024-03-20 14:35:09 +0100

    feat: Deleting Cart Items

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,4 +65,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req,res,next)=>{
     const productId = req.body.productId;
+    Product.deleteById(productId);
+    res.redirect('/admin/products');
 }

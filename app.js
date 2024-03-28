68b2b7d8380e7edaed6a631639096e3d312542c5
KotenkoNata <kotenkonata8@gmail.com>
2024-03-28 14:31:19 +0100

    feat: Adding a One-To-Many Relationship

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 require('dotenv').config();
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -21,8 +23,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
-    .sync()
+    .sync({force: true})
     .then(result => {
         app.listen(3000);
     })

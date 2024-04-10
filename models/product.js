c3789738d29b99bba3c3829d19647e3b1b963419
KotenkoNata <kotenkonata8@gmail.com>
2024-04-10 14:51:42 +0200

    feat: Fetching a Single Product

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
 const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 
 class Product {
     constructor(title, price, description, imageUrl) {
@@ -25,6 +26,18 @@ class Product {
                 return products;
             })
             .catch(err=>{console.log(err)});
+    }
+
+    static findById(prodId){
+        const db= getDb();
+        return db.collection('products')
+            .find({_id: new mongodb.ObjectId(prodId)})
+            .next()
+            .then((product)=>{
+                console.log(product);
+                return product;
+            })
+            .catch(err=>{console.log(err)})
     }
 }
 

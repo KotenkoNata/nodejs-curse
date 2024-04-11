a6139af237123bbe980ba4125547766edfe018ea
KotenkoNata <kotenkonata8@gmail.com>
2024-04-11 14:20:47 +0200

    feat: Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -76,14 +76,13 @@ exports.getProducts = (req, res, next) => {
         });
     }).catch(err=>console.log(err))
 };
-//
-// exports.postDeleteProduct = (req,res,next)=>{
-//     const productId = req.body.productId;
-//     Product.findByPk(productId).then(product=>{
-//         return product.destroy();
-//     }).then(result=>{
-//         console.log("destroy product")
-//         res.redirect('/admin/products');
-//     })
-//         .catch(err=>console.log(err));
-// }
+
+exports.postDeleteProduct = (req,res,next)=>{
+    const prodId = req.body.productId;
+    Product.deleteById(prodId)
+    .then(result=>{
+        console.log("destroy product")
+        res.redirect('/admin/products');
+    })
+        .catch(err=>console.log(err));
+}

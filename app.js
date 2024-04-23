dab8e154fd0283f0fdf0d53e817d47f510a558d9
KotenkoNata <kotenkonata8@gmail.com>
2024-04-23 14:37:33 +0200

    feat: Deleting Products

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.use(errorController.get404);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.5xgasid.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0`)
     .then(result=>
-        app.listen(3200)
+        app.listen(3000)
     )
     .catch(err=>{console.log(err)})

cad6d4d56c02362c9ee1412f26777b3bf022889e
KotenkoNata <kotenkonata8@gmail.com>
2024-04-23 14:48:48 +0200

    feat: Adding and Using a User Model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -15,18 +15,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const result = require("ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('661fc76e603f4d9f7068168e')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById('6627ad4d4099f4150ded3543')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +35,19 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.5xgasid.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0`)
-    .then(result=>
+    .then(result=>{
+        User.findOne().then(user=>{
+            if(!user){
+                const user = new User({
+                    name: 'Test user',
+                    email: 'test@test.com',
+                    cart: {
+                        items: [],
+                    }
+                })
+                user.save();
+            }
+        })
         app.listen(3000)
-    )
+    })
     .catch(err=>{console.log(err)})

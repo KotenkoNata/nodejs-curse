ab9e05703e6158997d6f9158592d53300a6a3cbc
KotenkoNata <kotenkonata8@gmail.com>
2024-05-01 14:48:05 +0200

    feat: Two Tiny Improvements

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.use(
     }));
 
 app.use((req, res, next) => {
+    if(!req.session.user){
+        return next();
+    }
     User.findById(req.session.user._id)
         .then(user => {
             req.user = user;

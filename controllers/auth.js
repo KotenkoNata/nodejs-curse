9b3245c8d9d1e812a88d15df8ab5575b729d5777
KotenkoNata <kotenkonata8@gmail.com>
2024-05-01 15:00:57 +0200

    feat: Updated Project Status

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,15 @@ exports.getLogin = (req, res, next) => {
             })
 }
 
+
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+};
+
 exports.postLogin = (req, res, next) => {
     User.findById('6627ad4d4099f4150ded3543')
         .then(user => {
@@ -20,6 +29,8 @@ exports.postLogin = (req, res, next) => {
         })
         .catch(err => console.log(err));
 }
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy((err)=>{

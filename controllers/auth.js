b81ea9bddc8e6d1fd9da28bb7c6d327ee1c6ffa9
KotenkoNata <kotenkonata8@gmail.com>
2024-05-15 14:36:12 +0200

    feat: Implementing the Token Logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendGridTransporter = require('nodemailer-sendgrid-transport');
@@ -123,5 +124,38 @@ exports.getReset = (req, res, next) => {
         errorMessage: message,
     });
 };
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buf) => {
+        if(err){
+            console.log(err);
+            return res.redirect('/reset')
+        }
+        const token = buf.toString('hex');
+        User.findOne({email: req.body.email})
+            .then((user) => {
+                if(!user){
+                    req.flash('error', 'No account with that email found.');
+                    return res.redirect('/reset');
+                }
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now()+ 3600000;
+                return user.save();
+            })
+            .then(result=>{
+                res.redirect('/');
+                transporter.sendMail({
+                    to: req.body.email,
+                    from: 'nataliiajuicy@gmail.com',
+                    subject: 'Password Reset',
+                    html: `
+                    <p>You requested password reset</p>
+                    <p>Click this <a href="http://localhost:3200/${token}">link</a> to set a new password.</p>
+                    `
+                });
+            })
+            .catch((err) => {console.log(err)})
+    })
+}
 
 

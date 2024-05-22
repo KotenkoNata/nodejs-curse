ad1a9a9e68854c9ce866419fc94eb55ee41027fa
KotenkoNata <kotenkonata8@gmail.com>
2024-05-22 13:44:34 +0200

    feat: Sanitizing Data

--- routes/auth.js
+++ routes/auth.js
@@ -13,10 +13,12 @@ router.get('/signup', authController.getSignup);
 router.post('/login',[
     body('email')
         .isEmail()
-        .withMessage('Please enter a valid email address'),
+        .withMessage('Please enter a valid email address')
+        .normalizeEmail(),
     body('password', 'Password has to be valid')
         .isLength({min: 5})
-        .isAlphanumeric(),
+        .isAlphanumeric()
+        .trim(),
 ], authController.postLogin);
 
 router.post('/signup',[
@@ -34,13 +36,15 @@ router.post('/signup',[
                     return Promise.reject('Email exists already, please pick a different one');
                 }
         })
-    })
+    }).normalizeEmail()
             , body(
         'password',
         'Please enter a password with only numbers and text and at least 5 characters.')
         .isLength({min:5})
-        .isAlphanumeric(),
-    body('confirmPassword').custom((value, {req})=>{
+        .isAlphanumeric().trim(),
+    body('confirmPassword')
+        .trim()
+        .custom((value, {req})=>{
         if(value !== req.body.password){
             throw new Error('Passwords have to match!');
         }

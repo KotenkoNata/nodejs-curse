57fbd459fae3a8e2d28b40edd2ffb7d70af81764
KotenkoNata <kotenkonata8@gmail.com>
2025-02-10 18:43:34 +0100

    feat: Uploading Images

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -21,16 +21,23 @@ exports.postPost = (req, res, next) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed, entered data is incorrect')
-    error.status = 422
+    error.statusCode = 422
     throw error
   }
 
+  if(!req.file){
+    const error = new Error('No image provided');
+    error.statusCode = 422;
+    throw error;
+  }
+
+  const imageUrl = req.file.path;
   const title = req.body.title
   const content = req.body.content
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/04fdb386a75cb4c9a2da4696187de8b4.jpg',
+    imageUrl: imageUrl,
     creator: {
       name: 'John',
     },

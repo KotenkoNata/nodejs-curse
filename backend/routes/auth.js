e7e2f57d5b783e46379b46fc10401c4cf7731760
KotenkoNata <kotenkonata8@gmail.com>
2025-02-12 16:53:28 +0100

    feat: Add Route for Status and Update Status

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const { body } = require('express-validator')
 
 const User = require('../models/user')
 const authController = require('../controllers/auth')
+const isAuth = require('../middleware/is-auth');
 
 router.put('/signup', [
   body('email')
@@ -28,5 +29,11 @@ router.put('/signup', [
 )
 
 router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch('/status', isAuth,[
+  body('status').trim().not().isEmpty(),
+], authController.updateUserStatus);
 
 module.exports = router

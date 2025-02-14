79ff23a5703023bf4cea97ec1837483b8e7ef774
KotenkoNata <kotenkonata8@gmail.com>
2025-02-14 15:43:58 +0100

    feat: Transforming "Then Catch" to "Async Await"

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const errors = validationResult(req);
 
   if(!errors.isEmpty()) {
@@ -18,99 +18,96 @@ exports.signup = (req, res, next) => {
   const password = req.body.password;
   const name = req.body.name;
 
-  bcrypt
-    .hash(password, 12)
-    .then(hashedPw=>{
-      const user = new User({
-        email: email,
-        password: hashedPw,
-        name: name,
-      })
-      return user.save();
+  try {
+    const hashedPw = await bcrypt
+      .hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      name: name,
     })
-    .then(result=>{
-      res.status(201).json({message:"User created", userId: result._id});
-    })
-    .catch(err=>{
+    const result = await user.save();
+
+    res.status(201).json({message:"User created", userId: result._id});
+  } catch (e) {
     if (!err.status) {
       err.statusCode = 500;
     }
     next(err);
-  });
+  }
 }
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   let loadedUser;
 
-  User.findOne({email:email})
-    .then(user=>{
-      if(!user){
-        const error = new Error('A user with that email does not exist');
-        error.statusCode = 401;
-        throw error;
-      }
+  try {
+    const user = await User.findOne({email:email});
+
+    if(!user) {
+      const error = new Error('A user with that email does not exist');
+      error.statusCode = 401;
+      throw error;
+    }
       loadedUser = user;
-      return bcrypt.compare(password, user.password);
-    }).then(isEqual=>{
+      const isEqual = await bcrypt.compare(password, user.password);
+
       if(!isEqual){
         const error = new Error('Wrong password');
         error.statusCode = 401;
         throw error;
       }
       const token = jwt.sign({
-        email: loadedUser.email,
-        userId: loadedUser._id.toString(),
-      },
+          email: loadedUser.email,
+          userId: loadedUser._id.toString(),
+        },
         `${process.env.JWT_SECRET}`,
         {expiresIn: '1h'}
       );
       res.status(200).json({token: token, userId: loadedUser._id.toString()});
-  })
-    .catch(err=>{
+  } catch (err) {
     if (!err.status) {
       err.statusCode = 500;
     }
     next(err);
-  })
+  }
 }
 
-exports.getUserStatus = (req, res, next) => {
-  User.findById(req.userId).then(user=>{
+exports.getUserStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId)
     if(!user){
       const error = new Error('User does not exist');
       error.statusCode = 404;
       throw error;
     }
     res.status(200).json({status: user.status});
-  }).catch(err=>{
+  } catch (err) {
     if(!err.statusCode){
       err.statusCode = 500;
     }
     next(err);
-  })
+  }
 }
 
-exports.updateUserStatus = (req, res, next) => {
+exports.updateUserStatus = async (req, res, next) => {
   const newStatus = req.body.status;
-  User.findById(req.userId)
-    .then(user=>{
-      if(!user){
-        const error = new Error('User does not exist');
-        error.statusCode = 404;
-        throw error;
-      }
-      user.status = newStatus;
-      return user.save();
-    })
-    .then(result=>{
-      res.status(200).json({message: 'User updated'});
-    })
-    .catch(err=>{
+
+  try {
+    const user = await User.findById(req.userId)
+    if(!user){
+      const error = new Error('User does not exist');
+      error.statusCode = 404;
+      throw error;
+    }
+    user.status = newStatus;
+    await user.save();
+    res.status(200).json({message: 'User updated'});
+  } catch (err) {
     if(!err.statusCode){
       err.statusCode = 500;
     }
     next(err);
-  })
+  }
 }

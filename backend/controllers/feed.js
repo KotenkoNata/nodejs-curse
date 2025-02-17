c37ff35aa7ccd0305571ba0cb247a931d6034346
KotenkoNata <kotenkonata8@gmail.com>
2025-02-17 15:59:25 +0100

    feat: Deleting Posts Across Clients

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -184,6 +184,7 @@ exports.deletePost = async (req, res, next) => {
 
     user.posts.pull(postId)
     await user.save()
+    io.getIO().emit('posts', {action: 'delete', post: postId})
 
     res.status(200).json({ message: 'Post Deleted Successfully' })
   }catch (err) {

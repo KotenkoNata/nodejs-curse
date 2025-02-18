cadb652b7957d5a2ec7097b19795392c917766c0
KotenkoNata <kotenkonata8@gmail.com>
2025-02-18 16:19:58 +0100

    feat: Adding a Create Post Mutation

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const User = require('../models/User');
+const Post = require('../models/Post');
 const jwt = require("jsonwebtoken");
 
 module.exports =  {
@@ -51,5 +52,34 @@ module.exports =  {
       email: user.email
     }, process.env.JWT_SECRET, {expiresIn: '1h'});
     return {token: token, userId: user._id.toString()};
+  },
+  createPost: async function({postInput}, req) {
+    const errors = [];
+    if(validator.isEmpty(postInput.title) || !validator.isLength(postInput.title, {min: 5})){
+      errors.push({ message: 'Title is invalid' });
+    }
+    if(validator.isEmpty(postInput.content) || !validator.isLength(postInput.content, {min: 5})){
+      errors.push({ message: 'Content is invalid' });
+    }
+    if(errors.length > 0){
+      const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+    })
+
+    const createdPost = await post.save();
+//TODO Add post to user's posts
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString(),
+    };
   }
 };

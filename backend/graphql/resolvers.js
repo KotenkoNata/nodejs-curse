8aca436cdf1d986e42ed4e3012e809589dbb7880
KotenkoNata <kotenkonata8@gmail.com>
2025-02-18 15:09:38 +0100

    feat: Adding Input Validation

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,8 +1,20 @@
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 const User = require('../models/User');
 
 module.exports =  {
   createUser: async function({userInput}, req) {
+    const errors = [];
+    if(!validator.isEmail(userInput.email)){
+      errors.push({ message: 'Email is invalid' });
+    }
+    if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min: 5})){
+      errors.push({ message: 'Password must be at least 6 characters' });
+    }
+    if(errors.length > 0){
+      const error = new Error('Invalid input');
+      throw error;
+    }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       const error = new Error('User already exists');

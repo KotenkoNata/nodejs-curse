3a03cb8973a785860c980ee3db085acda5ae6425
KotenkoNata <kotenkonata8@gmail.com>
2025-02-24 16:27:25 +0100

    feat: Testing Controllers

---
+++ test/auth-controller.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const User = require('../models/user');
+const AuthController = require('../controllers/auth');
+
+describe('Auth Controller - Login', () => {
+
+  it('should throw an error with code 500 if accessing the database fails', ()=>{
+    sinon.stub(User, 'findOne');
+    User.findOne.throws();
+
+    expect(AuthController.login);
+
+    User.findOne.restore();
+  })
+
+
+})

663171ccddf4c7b19e53d44dd350611661681d18
KotenkoNata <kotenkonata8@gmail.com>
2025-02-24 16:51:23 +0100

    feat: Setting up a Testing Database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
@@ -26,5 +27,22 @@ describe('Auth Controller - Login', () => {
     User.findOne.restore();
   })
 
+  it('should send a response with a valid user status for an existing user', (done)=>{
+    mongoose
+      .connect(
+        'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test-messages?retryWrites=true'
+      )
+      .then(result => {
+        const user = new User({
+          email: 'test@test.com',
+          password: 'test',
+          name: 'test',
+          posts: [],
+        });
+        return user.save();
+      })
+      .then()
+      .catch(err => console.log(err));
+  })
 
 })

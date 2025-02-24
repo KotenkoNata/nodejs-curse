ceca318c325296e246a5b61af2c695e100d76f18
KotenkoNata <kotenkonata8@gmail.com>
2025-02-24 15:50:42 +0100

    feat: Organizing Multiple Tests

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,11 +1,25 @@
 const expect = require('chai').expect;
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is provided', () => {
-  const req = {
-    get: function () {
-      return null;
+describe('Auth Middleware', () => {
+
+  it('should throw an error if no authorization header is provided', () => {
+    const req = {
+      get: function () {
+        return null;
+      }
     }
-  }
-  expect(authMiddleware.bind(this, req, {}, ()=>{})).to.throw('Not authenticated.');
+    expect(authMiddleware.bind(this, req, {}, ()=>{})).to.throw('Not authenticated.');
+  })
+
+  it('should throw an error if the authorization header is only one string', ()=>{
+    const req = {
+      get: function () {
+        return 'xyz';
+      }
+    }
+    expect(authMiddleware.bind(this, req, {}, ()=>{})).to.throw();
+  })
+
 })
+

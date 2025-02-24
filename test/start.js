634d0ba2641866b23cdc79618d5aab43e1262fca
KotenkoNata <kotenkonata8@gmail.com>
2025-02-24 15:46:26 +0100

    feat: Testing the Auth Middleware

--- test/start.js
+++ test/start.js
@@ -1,13 +1,7 @@
-const expect = require('chai').expect;
-
-it('should add numbers correctly', () => {
-  const num1 = 2;
-  const num2 = 3;
-  expect(num1+num2).to.eql(5);
-})
-
-it('should not give a result og 6', () => {
-  const num1 = 2;
-  const num2 = 3;
-  expect(num1+num2).not.to.eql(6);
-})
+// const expect = require('chai').expect;
+//
+// it('should add numbers correctly', () => {
+//   const num1 = 2;
+//   const num2 = 3;
+//   expect(num1+num2).to.eql(5);
+// })

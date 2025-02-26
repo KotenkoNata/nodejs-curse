9630e2ce16b75445fb4d1798730bab760c12a800
KotenkoNata <kotenkonata8@gmail.com>
2025-02-26 16:46:05 +0100

    feat: Adding REST Routes with TypeScript

--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
-import todosRouter from './routes/todos';
+import todosRoutes from './routes/todos';
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(todosRoutes);
 
 app.listen(3000);

07d72afe4ca64b8805cdd1d7d96c0d688d8062ac
KotenkoNata <kotenkonata8@gmail.com>
2025-02-27 14:55:00 +0100

    feat: Creating a Webserver

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const fs = require('fs').promises;
+const http = require('http');
 
-const text = 'This is a test - and it should be';
+const server = http.createServer((req, res) => {
+  res.end('Hello World! NODE');
+})
 
-fs.writeFile('node-message.txt', text)
-  .then(() => {
-    console.log('Successfully written! Node');
-  })
-  .catch((err) => {
-    console.error('Error writing file:', err);
-  });
+server.listen(8080);
